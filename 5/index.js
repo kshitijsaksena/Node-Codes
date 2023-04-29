416d138087f710b56419de9db8e4ace9f4b3b6ba
Kshitij <kshitijsaksena@gmail.com>
2023-04-29 13:49:20 +0530

    Remove Directories

--- 5/index.js
+++
@@ -1,9 +0,0 @@
-
-//Write a function in Node.js that accepts a command-line argument and logs it to the console.
-
-function getArgs(){
-    let args = process.argv.slice(2);
-    console.log(args)
-}
-
-getArgs();
